bcd457c1fdf7c0ecb57b695f4df39d6762f34ccd
E.A.T.I <eati@labs.cs.uns.edu.ar>
2023-03-02 15:42:04 -0300

    Adding main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Script utilizado para pasar los test de test.py
+"""
+import os
+
+
+def main():
+    """
+    Crea un directorio "build" y un archivo index.html dentro de el
+    """
+    os.makedirs('build', exist_ok=True)
+    with open('build/index.html', 'w', encoding='utf-8') as file_name:
+        file_name.write('')
+
+
+if __name__ == '__main__':
+    main()
